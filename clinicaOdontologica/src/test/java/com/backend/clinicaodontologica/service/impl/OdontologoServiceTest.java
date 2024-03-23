1e5c6c9c82fdb4f3042f5fff02e0e65b1cc585eb
ASUS <salvitanogonzalezgiuliana@gmail.com>
2024-03-23 10:36:17 -0300

    Implementando test para listar los odontólogos

--- clinicaOdontologica/src/test/java/com/backend/clinicaodontologica/service/impl/OdontologoServiceTest.java
+++ clinicaOdontologica/src/test/java/com/backend/clinicaodontologica/service/impl/OdontologoServiceTest.java
@@ -1,21 +1,14 @@
 package com.backend.clinicaodontologica.service.impl;
 
-import com.backend.clinicaOdontologica.dto.entrada.DomicilioEntradaDto;
 import com.backend.clinicaOdontologica.dto.entrada.OdontologoEntradaDto;
-import com.backend.clinicaOdontologica.dto.entrada.PacienteEntradaDto;
 import com.backend.clinicaOdontologica.dto.salida.OdontologoSalidaDto;
-import com.backend.clinicaOdontologica.dto.salida.PacienteSalidaDto;
 import com.backend.clinicaOdontologica.service.impl.OdontologoService;
-import com.backend.clinicaOdontologica.service.impl.OdontologoService;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
-import java.time.LocalDate;
+
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -44,5 +37,20 @@ class OdontologoServiceTest {
         assertEquals("Jose", odontologoSalidaDto.getNombre());
 
     }
+
+
+    @Test
+    @Order(2)
+    void deberiaDevolverUnaListaNoVaciaDePacientes() {
+        // Act
+        List<OdontologoSalidaDto> odontologos = odontologoService.listarOdontologos();
+
+
+        // Assert
+        Assertions.assertFalse(odontologos.isEmpty());
+    }
+
+
+
 
 }

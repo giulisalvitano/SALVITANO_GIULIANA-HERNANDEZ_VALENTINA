1652920ac57173d2314584e84252c29716f4948e
ASUS <salvitanogonzalezgiuliana@gmail.com>
2024-03-23 09:34:17 -0300

    Quedaron funcionando los test de Paciente

---
+++ clinicaOdontologica/src/test/java/com/backend/clinicaodontologica/service/impl/OdontologoServiceTest.java
@@ -0,0 +1,5 @@
+package com.backend.clinicaodontologica.service.impl;
+
+class OdontologoServiceTest {
+
+}
---
+++ clinicaOdontologica/src/test/java/com/backend/clinicaodontologica/service/impl/PacienteServiceTest.java
@@ -0,0 +1,63 @@
+package com.backend.clinicaodontologica.service.impl;
+
+import com.backend.clinicaOdontologica.dto.entrada.DomicilioEntradaDto;
+import com.backend.clinicaOdontologica.dto.entrada.PacienteEntradaDto;
+import com.backend.clinicaOdontologica.dto.salida.PacienteSalidaDto;
+import com.backend.clinicaOdontologica.service.impl.PacienteService;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestPropertySource(locations = "classpath:application-test.properties")
+class PacienteServiceTest {
+
+    @Autowired
+    private PacienteService pacienteService;
+
+
+    @Test
+    @Order(1)
+    void deberiaRegistrarseUnPacienteDeNombreJuan_yRetornarSuId() {
+        //arrange
+        PacienteEntradaDto pacienteEntradaDto = new PacienteEntradaDto("Juan", "Perez", 123456, LocalDate.of(2024, 3, 22), new DomicilioEntradaDto("Calle", 1234, "Localidad", "Provincia"));
+
+        //act
+        PacienteSalidaDto pacienteSalidaDto = pacienteService.registrarPaciente(pacienteEntradaDto);
+
+        //assert
+        assertNotNull(pacienteSalidaDto);
+        assertNotNull(pacienteSalidaDto.getId());
+        assertEquals("Juan", pacienteSalidaDto.getNombre());
+
+    }
+
+    @Test
+    @Order(2)
+    void deberiaEliminarseElPacienteConId1() {
+
+
+        assertDoesNotThrow(() -> pacienteService.eliminarPaciente(1L));
+    }
+
+
+    @Test
+    @Order(3)
+    void deberiaDevolverUnaListaVaciaDePacientes() {
+        List<PacienteSalidaDto> pacientes = pacienteService.listarPacientes();
+
+        assertTrue(pacientes.isEmpty());
+    }
+
+
+}

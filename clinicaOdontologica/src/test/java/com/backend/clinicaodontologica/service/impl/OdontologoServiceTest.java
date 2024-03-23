38e4705a2df4555cdee2f52b40ac86aab8068f1d
ASUS <salvitanogonzalezgiuliana@gmail.com>
2024-03-23 10:01:30 -0300

    Implementando test de Registrar Odontologo

--- clinicaOdontologica/src/test/java/com/backend/clinicaodontologica/service/impl/OdontologoServiceTest.java
+++ clinicaOdontologica/src/test/java/com/backend/clinicaodontologica/service/impl/OdontologoServiceTest.java
@@ -1,5 +1,48 @@
 package com.backend.clinicaodontologica.service.impl;
 
+import com.backend.clinicaOdontologica.dto.entrada.DomicilioEntradaDto;
+import com.backend.clinicaOdontologica.dto.entrada.OdontologoEntradaDto;
+import com.backend.clinicaOdontologica.dto.entrada.PacienteEntradaDto;
+import com.backend.clinicaOdontologica.dto.salida.OdontologoSalidaDto;
+import com.backend.clinicaOdontologica.dto.salida.PacienteSalidaDto;
+import com.backend.clinicaOdontologica.service.impl.OdontologoService;
+import com.backend.clinicaOdontologica.service.impl.OdontologoService;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestPropertySource(locations = "classpath:application-test.properties")
+
 class OdontologoServiceTest {
+
+    @Autowired
+    private OdontologoService odontologoService;
+
+    @Test
+    @Order(1)
+    void deberiaRegistrarseUnOdontologoConElNombreDeJose_yRetornarSuId() {
+        //arrange
+        OdontologoEntradaDto odontologoEntradaDto = new OdontologoEntradaDto("JP24O2000", "Jose", "Perez");
+
+        //act
+        OdontologoSalidaDto odontologoSalidaDto = odontologoService.guardarOdontologo(odontologoEntradaDto);
+
+        //assert
+        assertNotNull(odontologoSalidaDto);
+        assertNotNull(odontologoSalidaDto.getId());
+        assertEquals("Jose", odontologoSalidaDto.getNombre());
+
+    }
 
 }
